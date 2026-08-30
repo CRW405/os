5c32c65678e37c25bcd541ff0f85a10f21c75ea1
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-08-29 19:27:25 -0500

    ditched uefi and got to c via grub

--- C/src/kernel.c
+++ C/src/kernel.c
@@ -6,7 +6,7 @@ const unsigned short VGA_WHITE_ON_BLACK_STYLE = 0x0700;
 // no interrupts needed in protected mode
 volatile unsigned short *vga = (unsigned short *)0xB8000;
 
-void write_vga_line(int line, char *string) {
+void write_vga_line(int line, const char *string) {
 	for (int i = 0; string[i]; i++) {
 		// write each char into vga with white on black
 		vga[line * VGA_WIDTH + i] = (unsigned short)string[i] | VGA_WHITE_ON_BLACK_STYLE;
@@ -20,7 +20,7 @@ void clear_vga() {
 }
 
 void kernel_main(void) {
-	char *msg = "Hello, World! Goodbye. Space?";
+	const char *msg = "Hello, World! Goodbye. Space?";
 
 	clear_vga();
 	write_vga_line(VGA_HEIGHT / 2, msg);
